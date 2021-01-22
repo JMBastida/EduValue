dbb69ca874d750607af0f303774b4ee395743d9f
Jorge Martínez Bastida <jorgemartinezbastida@MacBook-Air-de-Jorge.local>
2021-01-22 09:41:45 +0100

    feat: 🎸 Filters for non universitary centers

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,29 +2,48 @@ import { Component, OnInit } from '@angular/core';
 import universities from '../../assets/universities.json';
 import { University } from '../shared/model/university.model';
 
-
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-  value = 0;
+  university: boolean = true;
   searchFilter: string = '';
-  constructor() { }
+  regions: string[] = [
+    'Comunitat Valenciana',
+    'Extremadura',
+    'Balears (Illes)',
+    'Cataluña',
+    'Andalucía',
+    'Galicia',
+    'Castilla y León',
+    'Madrid (Comunidad de)',
+    'Murcia (Región de)',
+    'Asturias (Principado de)',
+    'Canarias',
+    'Cantabria',
+    'País Vasco',
+    'Aragón',
+    'Estado',
+    'Navarra (Comunidad Foral de)',
+    'Castilla _ La Mancha',
+    'Rioja (La)',
+  ];
+
+  constructor() {}
 
   public universitiesList: University[] = universities;
 
   ngOnInit(): void {
-    this.value=10;
-    setTimeout(() => {
-      this.value=70;
-    }, 3000);
+    /*const unique = [
+      ...new Set(this.universitiesList.map((item) => item.comunidad_autonoma)),
+    ];
+    console.log(unique);*/
   }
 
   searchThis(data: any) {
-    console.log(data)
+    console.log(data);
     this.searchFilter = data;
   }
-
 }
--- src/app/shared/filters/non-university-filters/non-university-filters.component.ts
+++ src/app/shared/filters/non-university-filters/non-university-filters.component.ts
@@ -7,15 +7,90 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class NonUniversityFiltersComponent implements OnInit {
   @Output() searchcriteria = new EventEmitter<String>();
-  accordionStatus: boolean = false;
-
+  accordionStatus: boolean = true;
   searchText = '';
+  focus: any;
 
-  constructor() {}
+  comunidades = [
+    {
+      nombre: 'Andalucia',
+      provincias: [
+        'Almeria',
+        'Cadiz',
+        'Cordoba',
+        'Granada',
+        'Huelva',
+        'Jaen',
+        'Malaga',
+        'Sevilla',
+      ],
+    },
+    { nombre: 'Aragón', provincias: ['Huesca', 'Teruel', 'Zaragoza'] },
+    {
+      nombre: 'Canarias',
+      provincias: ['Las Palmas', 'Santa Cruz de Tenerife'],
+    },
+    { nombre: 'Cantabria', provincias: [''] },
+    {
+      nombre: 'Castilla y León',
+      provincias: [
+        'Avila',
+        'Burgos',
+        'Leon',
+        'Palencia',
+        'Salamanca',
+        'Segovia',
+        'Soria',
+        'Valladolid',
+        'Zamora',
+      ],
+    },
+    {
+      nombre: 'Castilla-La Mancha',
+      provincias: [
+        'Albacete',
+        'Ciudad Real',
+        'Cuenca',
+        'Guadalajara',
+        'Toledo',
+      ],
+    },
+    {
+      nombre: 'Cataluña',
+      provincias: ['Barcelona', 'Girona', 'Lleida', 'Tarragona'],
+    },
+    { nombre: 'Ceuta', provincias: [''] },
+    {
+      nombre: 'Comunidad Valenciana',
+      provincias: ['Alicante', 'Castellon', 'Valencia'],
+    },
+    { nombre: 'Comunidad de Madrid', provincias: [''] },
+    { nombre: 'Extremadura', provincias: ['Badajoz', 'Caceres'] },
+    {
+      nombre: 'Galicia',
+      provincias: ['A Coruña', 'Lugo', 'Ourense', 'Pontevedra'],
+    },
+    { nombre: 'Islas Baleares', provincias: [''] },
+    { nombre: 'La Rioja', provincias: [''] },
+    { nombre: 'Melilla', provincias: [''] },
+    { nombre: 'Navarra', provincias: [''] },
+    { nombre: 'País Vasco', provincias: ['Alava', 'Guipuzcoa', 'Vizcaya'] },
+    { nombre: 'Principado de Asturias', provincias: [''] },
+    { nombre: 'Región de Murcia', provincias: [''] },
+  ];
+  selectedRegion: number = 0;
+  selectedProvince: number = 0;
+
+
+  constructor() {
+  }
 
   ngOnInit(): void {}
 
+  accordionManage(event: boolean): void{
+    this.accordionStatus = event;
+  }
   searchForThis() {
-    this.searchcriteria.emit(this.searchText);
+    //api call
   }
 }
